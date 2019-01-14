678cdc3abe23bc3ce45aca13a43b2ccb72a8b854
Frey Simon <Simon.Frey@verbund.com>
2019-01-14 14:51:29 +0100

    Added head.list

--- R/functions.r
+++ R/functions.r
@@ -777,6 +777,45 @@ scalevalues <- function(x, ...){
 }
 
 
+#' Return the first or last part of a list
+#'
+#' Returns the first or last part of a list. Instead of returning the first
+#' n entries as the standard head() does, it attempts to call head()
+#' recursively on the entries in the list. If it fails, it will return the
+#' particular entry (standard behavior).
+#' @param obj a list object
+#' @param n a single integer. If positive, prints the first n items for the
+#' list and all entries in the list. If negative, prints all but the last
+#' n items in the list.
+#' @param ... additional parameters passed on to head()
+#' @author pimentel
+#' @references \link{https://gist.github.com/pimentel/256fc8c9b5191da63819}
+#' @export
+#' @examples 
+#' a <- list()
+#' a[[1]] <- c(1:100)
+#' a[[2]] <- c(500:1000)
+#' head.list(a)
+#' # same result
+#' head(a)
+#' @return a list of length n, with items in the list of length n
+head.list <- function(obj, n = 6L, ...)
+{
+  stopifnot(length(n) == 1L)
+  origN <- n
+  n <- if (n < 0L)
+    max(length(obj) + n, 0L)
+  else min(n, length(obj))
+  lapply(obj[seq_len(n)], function(x)
+  {
+    tryCatch({
+      head(x, origN, ...)
+    }, error = function(e) {
+      x
+    })
+  })
+}
+
 
 
 
