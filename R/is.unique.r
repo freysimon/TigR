849a77c81ba7936cf8d405f05252f8ae5656766f
Frey Simon <Simon.Frey@verbund.com>
2019-09-25 13:16:13 +0200

    Added modified version of is.unique Solved some bugs

--- R/is.unique.r
+++ R/is.unique.r
@@ -8,11 +8,14 @@
 #' @export
 #' @return a boolean vector
 #' @seealso \code{\link{unique}}
+#' 
 is.unique <- function(x, ...){
   library(plyr)
-  cx <- as.data.frame(matrix(data=NA, nrow = nrow(cx), ncol = 2))
+  countx <- count(x, ...length())
+  
+  cx <- as.data.frame(matrix(data=NA, nrow = nrow(countx), ncol = 2))
   colnames(cx) <- c("x","freq")
-  countx <- count(x, ...elt())
+  
   if(class(x) == "character"){
     cx[,1] <- as.character(countx[,1])
   } else if(class(x) %in% c("numeric","integer")){
@@ -22,7 +25,39 @@ is.unique <- function(x, ...){
   }
   
   cx[,2] <- as.integer(countx[,2])
-  cx[,2] == 1
   
+  return(cx[,2] == 1)
+  
+}
+
+#' Check for unique values
+#' @description Check whether a variable within a vetcor is unique
+#' @author Simon Frey
+#' @param x vector to be analysed.
+#' @param ... additional parameters passed to \code{\link{count}}
+#' @details This function uses the \code{\link{count}} function from the plyr package and returns all entries with a frquency greater than one. Works with character and numeric vectors
+#' @import plyr
+#' @export
+#' @return a vector containing the unique values of x
+#' @seealso \code{\link{unique}}
+#' 
+which.is.unique <- function(x, ...){
+  library(plyr)
+  countx <- count(x, ...)
+  
+  cx <- as.data.frame(matrix(data=NA, nrow = nrow(countx), ncol = 2))
+  colnames(cx) <- c("x","freq")
+  
+  if(class(x) == "character"){
+    cx[,1] <- as.character(countx[,1])
+  } else if(class(x) %in% c("numeric","integer")){
+    cx[,1] <- as.numeric(countx[,1])
+  } else {
+    cx[,1] <- as.factor(countx[,1])
+  }
+  
+  cx[,2] <- as.integer(countx[,2])
+  
+  return(cx[cx[,2] == 1,1])
   
 }
